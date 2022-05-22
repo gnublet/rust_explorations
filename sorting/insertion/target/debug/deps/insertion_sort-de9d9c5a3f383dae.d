fa4aeb614a428d2bb392e578268cb9765320b7b7
Kevin W <gnublet@gmail.com>
2022-05-22 02:28:20 -0700

    Removed target

--- sorting/insertion/target/debug/deps/insertion_sort-de9d9c5a3f383dae.d
+++
@@ -1,5 +0,0 @@
-/home/kevin/Documents/practice/rust/algorithms_and_data_structures/sorting/insertion/target/debug/deps/insertion_sort-de9d9c5a3f383dae: src/main.rs
-
-/home/kevin/Documents/practice/rust/algorithms_and_data_structures/sorting/insertion/target/debug/deps/insertion_sort-de9d9c5a3f383dae.d: src/main.rs
-
-src/main.rs:
--- sorting/insertion/target/debug/insertion_sort.d
+++
@@ -1,1 +0,0 @@
-/home/kevin/Documents/practice/rust/algorithms_and_data_structures/sorting/insertion/target/debug/insertion_sort: /home/kevin/Documents/practice/rust/algorithms_and_data_structures/sorting/insertion/src/main.rs
